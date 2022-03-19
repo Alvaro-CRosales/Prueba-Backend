3ef7333a87baf4dc2357d0fb0f965925905c44a2
Alvaro-CRosales <alvaro70001@gmail.com>
2022-03-18 20:00:52 -0600

    a

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,11 @@ import {app} from './app';
 import env from './config/env';
 import {connect} from './database/postgres'
 
-app.listen(env.PORT, (error?: Error) => {
+app.listen(process.env.PORT, (error?: Error) => {
   if (error) {
     console.log(error);
   } else {
-    console.log(`app running on port: ${env.PORT}`);
+    console.log(`app running on port: ${process.env.PORT}`);
   }
 });
 
